f47cee471bbe757fd93bcccdbef75fa293e6b33e
Daniel <danisolo91@gmail.com>
2021-09-03 15:39:34 +0200

    Add getters and setters

--- Floristeria/src/entity/Producte.java
+++ Floristeria/src/entity/Producte.java
@@ -5,8 +5,24 @@ public abstract class Producte {
 	protected String nom;
 	protected double preu;
 
-	public Producte(String nom, double preu) {
+	public Producte(String nom, double preu) throws Exception {
 		this.nom = nom;
+		this.preu = preu;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPreu() {
+		return preu;
+	}
+
+	public void setPreu(double preu) {
 		this.preu = preu;
 	}
 
