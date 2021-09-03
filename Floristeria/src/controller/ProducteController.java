ef6e1b2c9f5225ce2ec3d05663496c42ac9a6153
Daniel <danisolo91@gmail.com>
2021-09-03 19:03:42 +0200

    Create ProductController

---
+++ Floristeria/src/controller/ProducteController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import entity.Producte;
+import repository.ProducteRepository;
+import view.ProducteView;
+
+public class ProducteController {
+
+	private ProducteRepository producteRepository;
+	private ProducteView producteView;
+	
+	public ProducteController() {
+		producteRepository = new ProducteRepository();
+		producteView = new ProducteView();
+	}
+	
+	public void afegirArbre() {
+		Producte arbre = producteView.afegirArbre();
+		producteRepository.addProducte(arbre);
+	}
+	
+	public void afegirFlor() {
+		Producte flor = producteView.afegirFlor();
+		producteRepository.addProducte(flor);
+	}
+	
+	public void afegirDecoracio() {
+		Producte decoracio = producteView.afegirDecoracio();
+		producteRepository.addProducte(decoracio);
+	}
+	
+	public void mostrarStock() {
+		producteView.mostrarStock();
+	}
+}
