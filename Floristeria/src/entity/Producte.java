21c732b390c4e87ea8ce5dd293ddebee385d2f51
Daniel <danisolo91@gmail.com>
2021-09-03 12:22:24 +0200

    Add two attributes

--- Floristeria/src/entity/Producte.java
+++ Floristeria/src/entity/Producte.java
@@ -2,4 +2,12 @@ package entity;
 
 public abstract class Producte {
 
+	protected String nom;
+	protected double preu;
+
+	public Producte(String nom, double preu) {
+		this.nom = nom;
+		this.preu = preu;
+	}
+
 }
