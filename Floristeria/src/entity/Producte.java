9c0c2fe26a11d03a44a860a13770ec090d0a2b17
Daniel <danisolo91@gmail.com>
2021-09-03 16:41:03 +0200

    Update

--- Floristeria/src/entity/Producte.java
+++ Floristeria/src/entity/Producte.java
@@ -6,7 +6,7 @@ public abstract class Producte {
 	protected String nom;
 	protected double preu;
 
-	public Producte(String nom, double preu) throws Exception {
+	public Producte(String nom, double preu) {
 		this.nom = nom;
 		this.preu = preu;
 	}
