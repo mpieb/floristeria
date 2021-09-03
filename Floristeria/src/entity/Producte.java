d39d6c2e484e70863743ee4735c4af9dbdc4138b
Daniel <danisolo91@gmail.com>
2021-09-03 16:23:24 +0200

    Add ID attr and set/get

--- Floristeria/src/entity/Producte.java
+++ Floristeria/src/entity/Producte.java
@@ -2,12 +2,21 @@ package entity;
 
 public abstract class Producte {
 
+	protected int id;
 	protected String nom;
 	protected double preu;
 
 	public Producte(String nom, double preu) throws Exception {
 		this.nom = nom;
 		this.preu = preu;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getNom() {
