810fd53fd9716ead0e8d747a2527b616eb7dc358
Daniel <danisolo91@gmail.com>
2021-09-04 10:47:38 +0200

    Add toString methods

--- Floristeria/src/entity/Arbre.java
+++ Floristeria/src/entity/Arbre.java
@@ -3,21 +3,24 @@ package entity;
 public class Arbre extends Producte {
 	private double mida;
 
-	public Arbre(String nom, double mida, double preu ) throws Exception {
+	public Arbre(String nom, double mida, double preu) throws Exception {
 		super(nom, preu);
-        if (mida <= 0) throw new Exception();
-        this.mida = mida;
-    }
-	
-	public double getMida() {
-        return mida;
-    }
+		if (mida <= 0)
+			throw new Exception();
+		this.mida = mida;
+	}
 
-    public void setMida(double mida){
-        this.mida = mida;
-    }
+	public double getMida() {
+		return mida;
+	}
+
+	public void setMida(double mida) {
+		this.mida = mida;
+	}
+
+	@Override
+	public String toString() {
+		return "Arbre [id=" + id + ", nom=" + nom + ", preu=" + preu + ", mida=" + mida + "]";
+	}
 
 }
-
-
-
--- Floristeria/src/entity/Flor.java
+++ Floristeria/src/entity/Flor.java
@@ -3,20 +3,24 @@ package entity;
 public class Flor extends Producte {
 	private String color;
 
-    public Flor(String nom, String color, double preu) throws Exception {
-        super(nom, preu);
-        if (color.equals("")) throw new Exception();
-        this.color = color;
-    }
-    
-    public String getColor() {
-        return this.color;
-    }
+	public Flor(String nom, String color, double preu) throws Exception {
+		super(nom, preu);
+		if (color.equals(""))
+			throw new Exception();
+		this.color = color;
+	}
 
-    public void setColor(String color){
-        this.color = color;
-    }
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Flor [id=" + id + ", nom=" + nom + ", preu=" + preu + ", color=" + color + "]";
+	}
 
 }
-
-
