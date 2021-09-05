d0bcbf3ee43a6d890670f049cfe6f7e3c3e5344a
Miquel <76565907+mpieb@users.noreply.github.com>
2021-09-05 16:27:21 +0200

    Add files via upload

--- Floristeria/src/repository/TicketRepository.java
+++ Floristeria/src/repository/TicketRepository.java
@@ -6,16 +6,19 @@ import java.util.List;
 import entity.Ticket;
 
 public class TicketRepository {
-	
+		//Declaració i instanciació de llista Ticket
 		private List<Ticket> ticketList = new ArrayList<>();
-
+		
+		//Constructor per defecte
 		public TicketRepository(){
 		}
 		
+		//Getter
 		public List<Ticket> getAllTickets(){
 			return ticketList;
 		}
-				
+		
+		//Mètode per afegir tickets
 		public void addTicket(Ticket Ticket) {
 			ticketList.add(Ticket);
 		}		
